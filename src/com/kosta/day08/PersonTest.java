ec9d2ecc4b98f77abc709f36f5987669b315e15d
f1c04 <f1c04@icloud.com>
2021-02-20 22:14:03 +0900

    default

---
+++ src/com/kosta/day08/PersonTest.java
@@ -0,0 +1,10 @@
+package com.kosta.day08;
+
+import com.kosta.day05.Person;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person p = new Person();
+        System.out.println(p.COUNTRY);
+    }
+}
